d8150a9e6b4e744efbab2474e10a6fd3bf239af1
Pekka Tiitinen <pekka-1992@hotmail.com>
2022-02-16 14:17:34 +0100

    initial commit

---
+++ src/components/hero.js
@@ -0,0 +1,38 @@
+import React from "react"
+import styled from "styled-components"
+
+const StyledSection = styled.section`
+  .title {
+    margin-bottom: 0;
+  }
+  .subtitle {
+    margin-top: 0;
+  }
+  .highlighted {
+    box-shadow: inset 0 -2.5rem 0 #cdf3e1;
+  }
+`
+
+const Hero = () => {
+  return (
+    <StyledSection id="hero">
+      <h1 className="title">
+        Hello{" "}
+        <span role="img" aria-label="emoji">
+          👋
+        </span>
+        <br />
+        I'm Pekka Tiitinen
+      </h1>
+      <h2 className="subtitle">
+        I design and build{" "}
+        <span className="highlighted">things for the web</span>.
+      </h2>
+      <div className="description">
+        Product Manager and Freelance Web Developer. Based in Madrid.
+      </div>
+    </StyledSection>
+  )
+}
+
+export default Hero
